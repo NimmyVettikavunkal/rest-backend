9ad9999eec00852607a2b7ac41f816b61d2da63b
user <example@gmail.com>
2023-10-11 14:58:39 +0000

    used clientRepository java class to get all the client details

--- src/main/java/com/example/restbackend/controller/ClientController.java
+++ src/main/java/com/example/restbackend/controller/ClientController.java
@@ -1,6 +1,8 @@
 package com.example.restbackend.controller;
 
 import com.example.restbackend.model.Client;
+import com.example.restbackend.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,14 +11,12 @@ import java.util.List;
 @RestController
 public class ClientController {
 
+    @Autowired
+    ClientRepository clientRepository;
     @GetMapping("/client")
     public List<Client> getAllClients() {
-        List<Client> client = List.of(
-                new Client("Jane","23/8/1956"),
-                new Client("Mary","17/9/1945"),
-                new Client("Henry", "9/2/1963")
-        );
-        return client;
+
+        return clientRepository.findAll();
     }
 
 }
