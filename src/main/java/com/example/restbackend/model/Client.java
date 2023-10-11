5054fa76ee47c89e77b22eed6673b6ae5fa1cc54
user <example@gmail.com>
2023-10-11 13:48:37 +0000

    Generated the fields for client model class

---
+++ src/main/java/com/example/restbackend/model/Client.java
@@ -0,0 +1,22 @@
+package com.example.restbackend.model;
+
+public class Client {
+
+    private Long clientId;
+
+    private String name;
+    private String dob;
+    private String ppsn;
+    private String gender;
+    private String clientAddress;
+    private int clientContactNo;
+    private String doctorName;
+    private String doctorAddress;
+
+    private int contactNo1;
+    private int contactNo2;
+
+    public Client() {
+    }
+
+}
