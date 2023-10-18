f5cdd42fdaa2c1a6b66fa36cbca40616a2b7a6a8
user <example@gmail.com>
2023-10-18 09:29:42 +0000

    PostMapping added to controller class

--- src/main/java/com/example/restbackend/controller/ClientController.java
+++ src/main/java/com/example/restbackend/controller/ClientController.java
@@ -4,6 +4,8 @@ import com.example.restbackend.model.Client;
 import com.example.restbackend.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,5 +20,11 @@ public class ClientController {
 
         return clientRepository.findAll();
     }
+    @PostMapping("/client")
+    public Client createClient(@RequestBody Client newClient) {
+        // Assuming you have a clientRepository for data access
+        return clientRepository.save(newClient);
+    }
+
 
 }
